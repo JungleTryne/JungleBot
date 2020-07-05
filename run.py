4f006f4b55142e68a00d8677b12d27a5baa35372
Mishin Danila <danila.mishin.2001@gmail.com>
2020-07-05 12:58:15 +0300

    added a simple Cog

--- run.py
+++ run.py
@@ -2,6 +2,11 @@ from core import JungleClient
 
 from secret import BOT_TOKEN
 
+from BotCogs.greeting import Greetings
+import discord
+from discord.ext import commands
+
 if __name__ == '__main__':
-    client = JungleClient()
-    client.run(BOT_TOKEN)
+    bot = JungleClient('.')
+    bot.add_cog(Greetings(bot))
+    bot.run(BOT_TOKEN)
