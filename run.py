195920948e51b9d4786f62b9d4b3027d5c30d019
Mishin Danila <danila.mishin.2001@gmail.com>
2020-07-07 18:15:59 +0300

    local merge

--- run.py
+++ run.py
@@ -1,7 +1,21 @@
-from core import JungleClient
+import os
 
+from core import JungleClient
 from secret import BOT_TOKEN
+from BotCogs import greeting, admin
+
+
+absolute_path = os.path.dirname(os.path.abspath(__file__))
+
+modules = [
+    greeting,
+    admin,
+]
 
 if __name__ == '__main__':
-    client = JungleClient()
-    client.run(BOT_TOKEN)
+    bot = JungleClient('j.')
+
+    for module in modules:
+        module.setup(bot)
+
+    bot.run(BOT_TOKEN)
